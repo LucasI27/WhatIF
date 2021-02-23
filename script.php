1858c808b93a6dec7016af82a704ee4fb3514f1b
LucasI27 <luina.12042003@gmail.com>
2021-02-23 18:12:21 -0300

    script.php
    separei o script do html e linkei,
    comecei o header tbm mas ainda não ta pronto

---
+++ script.php
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+                
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text='+$value,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
