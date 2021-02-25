7ba97ab2b358198babda1722f0316c689bb142e4
Daniel_web <dalvescruz3002@gmail.com>
2021-02-25 18:43:54 -0300

    Chatbot

--- message.php
+++ message.php
@@ -1,5 +1,7 @@
 <?php
 
+echo $_POST["pergunta"];
+
 $conn = mysqli_connect("localhost", "root", "", "bot") or die("Database Error");
 
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
@@ -11,6 +13,10 @@ if(mysqli_num_rows($run_query) > 0){
     $fetch_data = mysqli_fetch_assoc($run_query);
     $replay = $fetch_data['replies'];
     echo $replay;
+
+}else if($_POST["data"] == "pergunta"){
+    echo "<img src='imgs/inf.png' width=320px height=260px />";
+
 }else{
     echo "Desculpa, não consigo entender, tente fazer uma pergunta mais simples.";
 }
